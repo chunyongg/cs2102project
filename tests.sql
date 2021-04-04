4137f8919f02bea1d916716e7acc22ccbc1ff624
Cy <chunyonggg@gmail.com>
2021-04-04 22:43:21 +0800

    Refactor

--- tests.sql
+++ tests.sql
@@ -1,7 +1,12 @@
-<<<<<<< HEAD
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- CHUN YONG'S TESTS
+
 -- Should fail
 -- No such course area
 CALL add_course('EC1101', 'Learn all about Economics', 'Economics', 5);
+CALL add_course('GD1101', 'Learn all about Games', 'Game Design', 5); -- Pass
 --manager must be full time
 CALL add_employee('part_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'manager', '{"Game Design"}');
 -- admin must be full time
@@ -10,6 +15,9 @@ CALL add_employee('part_time', 'John', 'Potato Street', 93342258, 'johny@gmail.c
 CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'administrator', '{"Game Design"}');
 -- Manager must manage at least one area
 CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'manager', '{}');
+CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'manager', '{"Game Design"}'); -- Pass
+CALL add_employee('full_time', 'Maynard', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'instructor', '{"Game Design"}'); -- Pass
+CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'administrator', '{}'); -- Pass
  -- Manager managing course areas cannot be removed
 CALL remove_employee(43, '2021-02-21')
 -- Employee already left
@@ -19,23 +27,7 @@ CALL add_course_offering(11, 1, 300, 10, '2021-05-21', '2021-05-11', 42, '{}' ::
 -- Session capacity less than target number
 CALL add_course_offering(11, 1, 300, 200, '2021-05-21', '2021-05-11', 42, '{"(2021-05-21,\"2021-05-21 00:00:00\",1)"}' :: SessionInfo[])
 -- Should pass
-CALL add_course('GD1101', 'Learn all about Games', 'Game Design', 5);
-CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'manager', '{"Game Design"}');
-CALL add_employee('full_time', 'Maynard', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'instructor', '{"Game Design"}');
-CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'administrator', '{}');
 CALL add_course_offering(11, 1, 300, 10, '2021-05-21', '2021-05-11', 42, '{"(2021-05-21,\"2021-05-21 00:00:00\",11)"}' :: SessionInfo[])
-
-=======
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- CHUN YONG'S TESTS
-
--- F12
--- select func(...) - returns 0
--- select func(...) - returns 0
-
--- F14
--- select func(...) - returns 0
--- select func(...) - returns 0
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- RUI EN's TESTS
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -43,4 +35,3 @@ CALL add_course_offering(11, 1, 300, 10, '2021-05-21', '2021-05-11', 42, '{"(202
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- MICH's TESTS
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
->>>>>>> main
