f4c1d8969a071c2b80201cd9c93de09a0742834d
Cy <chunyonggg@gmail.com>
2021-04-04 22:45:42 +0800

    Adhere to green blocks

--- tests.sql
+++ tests.sql
@@ -4,9 +4,8 @@
 -- CHUN YONG'S TESTS
 
 -- Should fail
--- No such course area
-CALL add_course('EC1101', 'Learn all about Economics', 'Economics', 5);
-CALL add_course('GD1101', 'Learn all about Games', 'Game Design', 5); -- Pass
+
+-- F1
 --manager must be full time
 CALL add_employee('part_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'manager', '{"Game Design"}');
 -- admin must be full time
@@ -18,10 +17,18 @@ CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.c
 CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'manager', '{"Game Design"}'); -- Pass
 CALL add_employee('full_time', 'Maynard', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'instructor', '{"Game Design"}'); -- Pass
 CALL add_employee('full_time', 'John', 'Potato Street', 93342258, 'johny@gmail.com', 3321.33, '2009-12-31', 'administrator', '{}'); -- Pass
+
+--F2
  -- Manager managing course areas cannot be removed
 CALL remove_employee(43, '2021-02-21')
 -- Employee already left
 CALL remove_employee(40, '2021-05-21')
+
+--F5 
+CALL add_course('EC1101', 'Learn all about Economics', 'Economics', 5); -- No such course area
+CALL add_course('GD1101', 'Learn all about Games', 'Game Design', 5); -- Pass
+
+--F10
 -- Sessions cannot be empty
 CALL add_course_offering(11, 1, 300, 10, '2021-05-21', '2021-05-11', 42, '{}' :: SessionInfo[])
 -- Session capacity less than target number
