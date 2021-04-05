91388e276fca22b609c62b1c6be6cbd211e98214
Cy <chunyonggg@gmail.com>
2021-04-05 21:51:51 +0800

    Add triggers

--- proc.sql
+++ proc.sql
@@ -18,12 +18,10 @@ hour_rate numeric(10,2),
 monthly_salary numeric(10,2), 
 salary_earned numeric(10,2)
 ) AS $$
-BEGIN 
     SELECT * FROM pay_fullTimeEmployees() 
     UNION 
     SELECT * FROM pay_PartTimeEmployees();
-END;
-$$ LANGUAGE PLPGSQL;
+$$ LANGUAGE SQL;
 
 
 CREATE OR REPLACE FUNCTION pay_PartTimeEmployees()
@@ -44,12 +42,12 @@ BEGIN
     SELECT end_of_month(current_date) into month_end;
     FOR r IN SELECT * FROM PartTimeEmployees FE NATURAL JOIN EMPLOYEES E 
     WHERE E.depart_date IS NULL 
-    OR date_part('month', E.depart_date) >= date_part('month', curr_date)
+    OR date_part('month', E.depart_date) >= date_part('month', current_date)
     LOOP 
         emp_id := r.emp_id;
         name := r.emp_name;
-        status := 'part_time';
-        worked_hours := find_hours_worked(current_date, emp_id);
+        status := 'Part Time';
+        worked_hours := coalesce(find_hours_worked(current_date, emp_id), 0);
         hour_rate := r.hourly_rate;
         salary_earned := worked_hours * hour_rate;
         INSERT INTO PartTimeSalary VALUES(salary_earned, month_end, worked_hours, emp_id);
@@ -77,7 +75,6 @@ month_end date;
 last_work_day integer;
 first_work_day integer;
 total_days_in_month integer;
-salary_earned numeric(10,2);
 BEGIN 
     SELECT current_date INTO curr_date;
     SELECT end_of_month(current_date) into month_end;
@@ -87,7 +84,7 @@ BEGIN
         LOOP 
             emp_id := r.emp_id;
             name := r.emp_name;
-            status := "full_time";
+            status := 'Full Time';
             monthly_salary := r.monthly_salary;
 
             SELECT calculate_last_work_day(r.depart_date) INTO last_work_day;
@@ -109,7 +106,7 @@ RETURNS INTEGER AS $$
 DECLARE 
 BEGIN 
             IF (date_part('month', j_date) = date_part('month', current_date)) THEN 
-                RETURN j_date;
+                RETURN EXTRACT(DAY FROM j_date);
             ELSE 
                 RETURN 1;
             END IF;
@@ -164,12 +161,30 @@ BEGIN
         RETURN NULL;
     END IF;
         new_hours_worked := get_difference_in_hours(NEW.end_time, NEW.start_time);
-            UPDATE PartTimeHoursWorked 
-    SET hours_worked = hours_worked + new_hours_worked
-    WHERE emp_id = inst_id AND month_year = date_trunc('month', NEW.sess_date);
+        INSERT INTO PartTimeHoursWorked 
+        VALUES (new_hours_worked, date_trunc('month', NEW.sess_date), inst_id)
+        ON CONFLICT (month_year, emp_id) DO UPDATE SET hours_worked = EXCLUDED.hours_worked + new_hours_worked;
     RETURN NULL;
 END;
 $$ LANGUAGE PLPGSQL;
+
+CREATE OR REPLACE FUNCTION removeHoursWorked_partTimeInstructor()
+RETURNS TRIGGER AS $$
+DECLARE 
+inst_id integer;
+old_hours_worked integer;
+BEGIN 
+    inst_id := OLD.instructor_id;
+        IF (NOT EXISTS (SELECT 1 FROM PartTimeInstructors WHERE emp_id = inst_id)) THEN 
+        RETURN NULL;
+    END IF;
+        old_hours_worked := get_difference_in_hours(OLD.end_time, OLD.start_time);
+        UPDATE PartTimeHoursWorked PTH
+        SET PTH.hours_worked = PTH.hours_worked - old_hours_worked
+        WHERE PTH.emp_id = inst_id AND PTH.month_year = date_trunc('month', OLD.sess_date);
+    RETURN NULL;
+END;
+$$ LANGUAGE PLPGSQL
 
 CREATE OR REPLACE FUNCTION updateHoursWorked_partTimeInstructor()
 RETURNS TRIGGER AS $$
