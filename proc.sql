7bdda770cc9a4da0663f415df79dbe714b41503e
Cy <chunyonggg@gmail.com>
2021-04-06 21:20:49 +0800

    Triggers

--- proc.sql
+++ proc.sql
@@ -2,8 +2,26 @@
 -- TRIGGERS AND THEIR FUNCTIONS (put as a pair!)
 
 -- Trigger 1
-CREATE OR REPLACE FUNCTION after_redeem_session_func()
-CREATE TRIGGER after_redeem_session_trigger
+CREATE OR REPLACE FUNCTION before_register_check_has_not_registered()
+RETURNS TRIGGER AS $$
+BEGIN 
+    IF EXISTS (SELECT 1 FROM SessionParticipants WHERE cust_id = NEW.cust_id AND sess_id = NEW.sess_id) THEN 
+        RAISE EXCEPTION 'Already registered for session';
+    END IF;
+    RETURN NEW;
+END; 
+$$ LANGUAGE PLPGSQL;
+
+DROP TRIGGER IF EXISTS before_register_check_has_not_registered ON REGISTERS;
+CREATE TRIGGER before_register_check_has_not_registered
+BEFORE INSERT ON REGISTERS 
+FOR EACH ROW EXECUTE FUNCTION before_register_check_has_not_registered();
+
+DROP TRIGGER IF EXISTS before_redeem_check_has_not_registered ON REDEEMS;
+CREATE TRIGGER before_redeem_check_has_not_registered
+BEFORE INSERT ON REGISTERS 
+FOR EACH ROW EXECUTE FUNCTION before_register_check_has_not_registered();
+
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- GLOBAL UTILITY FUNCTIONS (place functions that you think can help everyone here!)
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
