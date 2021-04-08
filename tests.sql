f9c55638da5c7c1063097d6360e383ee07c114c2
Chun Yong <chunyonggg@gmail.com>
2021-04-08 17:31:47 +0800

    Update

--- tests.sql
+++ tests.sql
@@ -11,6 +11,11 @@ CALL add_session(1, 2, '2021-04-03', '2021-05-03 17:00', 25, 7) -- Session in pa
 CALL add_session(1, 2, '2021-04-05', '2021-05-03 17:00', 25, 7) -- Instructor does not specialize in area
 CALL add_session(1, 2, '2021-07-01', '2021-07-01 10:00', 25, 6) -- Room occupied
 CALL add_session(8, 4, '2021-08-01','2021-07-01 14:00' , 27, 6); -- Instructor left
+
+CALL add_session(21, 2, '2021-05-03', '2021-05-03 14:00', 29, 7) 
+CALL remove_session(21, 2)
+-- Check course offering seating capacity afterwards
+
 CALL add_session(7, 4, '2021-07-01','2021-07-01 14:00' , 30, 6); --Note: Replace with latest session number, should succeed
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 -- RUI EN's TESTS
