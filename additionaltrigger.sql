164c44376252b1ae9ea64ed6b75954efa6841e35
Xin Yee <codelearner19@gmail.com>
2021-04-10 19:17:46 +0800

    Add check cc exist trigger

--- additionaltrigger.sql
+++ additionaltrigger.sql
@@ -47,3 +47,28 @@ FOR EACH ROW EXECUTE FUNCTION check_cc_expiry();
 CREATE TRIGGER reg_check_cc_trigger
 BEFORE INSERT OR UPDATE ON Registers
 FOR EACH ROW EXECUTE FUNCTION check_cc_expiry();
+
+-- check credit card valid 
+
+CREATE OR REPLACE FUNCTION check_cc_exist() RETURNS TRIGGER AS $$
+BEGIN
+    if not exists (select CC.cust_id
+        from CreditCards CC 
+        where CC.cust_id = New.cust_id 
+        and CC.cc_number = New.cc_number) then
+        raise exception 'No such Credit Card exist under the Customer, please check again.';
+    else
+        return New;
+    end if;
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER buy_check_cc_exist_trigger
+BEFORE INSERT OR UPDATE ON Buys
+FOR EACH ROW EXECUTE FUNCTION check_cc_exist();
+
+CREATE TRIGGER reg_check_cc_exist_trigger
+BEFORE INSERT OR UPDATE ON Registers
+FOR EACH ROW EXECUTE FUNCTION check_cc_exist();
+
+
